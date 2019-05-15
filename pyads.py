b23a563b0aeb76133c8d777390ea1b8edca11fe6
Akke Viitanen <akke.viitanen@helsinki.fi>
2019-05-15 13:35:39 +0300

    Intermediate commit

--- pyads.py
+++ pyads.py
@@ -27,8 +27,15 @@ import ads
 
 DESCRIPTION = "Query the ADS database."
 
+GROUPS = (
+        {"title": "Query arguments",
+         "description": "Arguments for ADS query control"},
+        {"title": "Output arguments",
+         "description": "Arguments for output control"}
+)
+
 # Set up the command line options
-ARGUMENTS = (
+ARGUMENTS_QUERY = (
     (["-a", "--author"], {"nargs": '?',
                           "type": str,
                           "help": "Author search string e.g. doe, john."}),
@@ -51,17 +58,41 @@ ARGUMENTS = (
                         "help": "Year search string e.g. 2000-2001."}),
 )
 
+# Arguments to control the output formatting
+ARGUMENTS_OUTPUT = (
+    (["--print_one_per_line"], {"action": "store_true",
+                                "help": "Print one line of output for each query result"}),
+    (["--print_first_author"], {"action": "store_true",
+                                "help": "Print the first author."}),
+    (["--print_authors"], {"action": "store_true",
+                           "help": "Print all of the authors."}),
+    (["--print_bibstem"], {"action": "store_true",
+                           "help": "Print the bibstem, e.g. A&A"}),
+    (["--print_year"], {"action": "store_true",
+                        "help": "Print the year."}),
+    (["--print_abs"], {"action": "store_true",
+                       "help": "Print the full abstract."}),
+    (["--print_url_abs"], {"action": "store_true",
+                           "help": "Print the ADS URL for the abstract."}),
+    (["--print_url_pdf"], {"action": "store_true",
+                           "help": "Print the ADS URL for the downloadables."}),
+)
+ARGUMENTS = ARGUMENTS_QUERY, ARGUMENTS_OUTPUT
+
 # Parse the command line arguments
 PARSER = argparse.ArgumentParser(description=DESCRIPTION)
-for args, kwargs in ARGUMENTS:
-    PARSER.add_argument(*args, **kwargs)
+for group, arguments in zip(GROUPS, ARGUMENTS):
+    _group = PARSER.add_argument_group(**group)
+    for args, kwargs in arguments:
+        _group.add_argument(*args, **kwargs)
 ARGS = PARSER.parse_args()
+print(ARGS)
 
 # Build the query dictionary i.e. filter out `none` values
-QUERY_DICT = {k: v for k, v in ARGS.__dict__.items() if v}
+#QUERY_DICT = {k: v for k, v in ARGS.__dict__.items() if v}
 
 # Include these fields in the query result
-FIELDS = ["first_author", "bibcode", "title", "year"]
+FIELDS = ["first_author", "bibcode", "title", "year", "bibtex"]
 
 def print_header():
     """
@@ -82,19 +113,22 @@ def print_row(paper):
              paper.year,
              func_trunc(paper.title[0], 100)))
 
+def print_bibtex(paper):
+    print(paper.bibtex)
+
 def main():
     """
     Query ADS for the search string and print formatted results.
     """
 
     # Check for empty dictionary
-    if (len(QUERY_DICT) == 2
-        and ("sort" in QUERY_DICT)
-        and "rows" in QUERY_DICT):
-        print("Error: At least one search parameter must be provided.",
-              file=sys.stderr)
-        PARSER.print_usage()
-        return 0
+    #if (len(QUERY_DICT) == 2
+    #    and ("sort" in QUERY_DICT)
+    #    and "rows" in QUERY_DICT):
+    #    print("Error: At least one search parameter must be provided.",
+    #          file=sys.stderr)
+    #    PARSER.print_usage()
+    #    return 0
 
     # Query the ADS database
     query = ads.SearchQuery(fl=FIELDS, **QUERY_DICT)
@@ -102,7 +136,8 @@ def main():
     # Print the output
     print_header()
     for paper in query:
-        print_row(paper)
+#        print_row(paper)
+        print_bibtex(paper)
 
     # Print useful information about remaining quota and reset time
     print("", file=sys.stderr)
